b619fec30b4fadef4df4f12b7c9c9f0be3ea55ae
ramiali <ramiali19989@gmail.com>
2021-09-12 20:10:33 +0300

    rami add orders

--- src/entity/Customers.ts
+++ src/entity/Customers.ts
@@ -1,4 +1,4 @@
-import {Entity, PrimaryGeneratedColumn, Column, BaseEntity, Timestamp, OneToMany} from "typeorm"; 
+import {Entity, PrimaryGeneratedColumn, Column, BaseEntity, Timestamp, OneToMany, CreateDateColumn} from "typeorm"; 
 import { OrderItem } from "./Order";
 
 @Entity()
@@ -7,12 +7,14 @@ export class Customers extends BaseEntity{
     @PrimaryGeneratedColumn()
     CustomerId: number;
     @Column()
+    CustomerName: String;  
+    @Column()
     phone: string;
     @Column()
     total_cost: string;
-    @Column()
+    @Column({default:false})
     notified: boolean;
-    @Column()
+    @CreateDateColumn()
     CreatedAt:Date;
  
     @OneToMany(type => OrderItem, orderItem => orderItem.customer,{cascade:true})
